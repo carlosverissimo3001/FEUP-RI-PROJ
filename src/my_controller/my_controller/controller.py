194fa3467cdbff404692aaac7497c4481dab28e7
Rafael Camelo <Rafael77001@hotmail.com>
2023-10-24 11:07:19 +0100

    formatting

--- src/my_controller/my_controller/controller.py
+++ src/my_controller/my_controller/controller.py
@@ -30,9 +30,11 @@ ERROR_OFFSET = 0.01
 
 DEBUG_MODE = True
 
+
 def debug_print(string):
     if DEBUG_MODE:
         print(string)
+
 
 class SpinRandomlyNode(Node):
     def __init__(self):
@@ -52,13 +54,14 @@ class SpinRandomlyNode(Node):
 
     def scan_callback(self, scan_msg: LaserScan):
         cmd_vel = Twist()
-        
+
         # just spin randomly
         cmd_vel.linear.x = 0.0;
         cmd_vel.angular.z = float(random.uniform(-1, 1))
 
         # Publish the message
         self.publisher.publish(cmd_vel)
+
 
 class ScanToVelocityNode(Node):
     def __init__(self):
@@ -67,15 +70,15 @@ class ScanToVelocityNode(Node):
         # create a dictionair, if the value is from 0 to 60, then it's on the left
         # if the value is from 60 to 120, then it's in the front, you get it
         self.directions = {
-            "front" : list(range(0, 22)) + list(range(337, 360)),
-            "front-top" : range(22, 67),
-            "top" : range(67, 112),
-            "top-back" : range(112, 157),
-            "back" : range(157, 202),
-            "back-bottom" : range(202, 247),
-            "bottom" : range(247, 292),
-            "bottom-front" : range(292, 337),
-        } 
+            "front": list(range(0, 22)) + list(range(337, 360)),
+            "front-top": range(22, 67),
+            "top": range(67, 112),
+            "top-back": range(112, 157),
+            "back": range(157, 202),
+            "back-bottom": range(202, 247),
+            "bottom": range(247, 292),
+            "bottom-front": range(292, 337),
+        }
 
         # Subscribe to the /scan topic
         self.subscription = self.create_subscription(
@@ -97,31 +100,29 @@ class ScanToVelocityNode(Node):
         if (math.isinf(min(scan_data.ranges)) and math.isinf(max(scan_data.ranges))):
             print("I'm lost")
             return True
-        
+
         return False
-    
+
     def check_final_position(self, ranges):
         # Get the indexes of the sensors with data
         indexes = [x for x in range(len(ranges)) if not math.isinf(ranges[x])]
-        
+
         # get indexes
-        leftest_sensor = min(indexes)    
+        leftest_sensor = min(indexes)
         rightest_sensor = max(indexes)
-    
+
         # get distances
         leftest_sensor_dist = ranges[leftest_sensor]
         rightest_sensor_dist = ranges[rightest_sensor]
 
         if abs(leftest_sensor_dist - rightest_sensor_dist) > 10 * ERROR_OFFSET:
             return False
-        
 
         middle_angle = abs(leftest_sensor - rightest_sensor)
         left_angle = (180 - middle_angle) / 2
         right_angle = (180 - middle_angle) / 2
 
-
-        if (middle_angle > 180):
+        if middle_angle > 180:
             return False
 
         wall_length = leftest_sensor_dist * math.sin(math.radians(middle_angle)) / math.sin(math.radians(left_angle))
@@ -134,10 +135,6 @@ class ScanToVelocityNode(Node):
 
         return False
 
-        #print("Min index: ", min_index)
-        #print("Max index: ", max_index)
-
-        
     def get_closest_wall_direction(self, scan_data):
         # get the index of the closest obstacle
         min_range_index = scan_data.ranges.index(min(scan_data.ranges))
@@ -150,7 +147,7 @@ class ScanToVelocityNode(Node):
     def get_closest_wall_degree(self, ranges):
         # get the index of the closest obstacle
         return ranges.index(min(ranges))
-    
+
     def follow_wall(self, ranges):
         print("\nInside follow_wall")
 
@@ -159,25 +156,24 @@ class ScanToVelocityNode(Node):
             print("Closest wall index: ", closest_sensor)
             print("Left sensor data is inf")
 
-            if closest_sensor > 270 or closest_sensor < 90: 
+            if closest_sensor > 270 or closest_sensor < 90:
                 return 0.0, -1 * NORMAL_ANGULAR_VELOCITY
-            
-            return 0.0, NORMAL_ANGULAR_VELOCITY            
-                            
-        # if the left sensor is too close to the wall
+
+            return 0.0, NORMAL_ANGULAR_VELOCITY
+
+            # if the left sensor is too close to the wall
         if ranges[SCAN_LEFT] < 0.5 * APPROACH_THRESHOLD:
             # rotate left
             print("rotate left")
             return NORMAL_LINEAR_VELOCITY, -1 * NORMAL_ANGULAR_VELOCITY
 
-
-        parallel_to_wall = abs(ranges[SCAN_LEFT - 1] - ranges[SCAN_LEFT + 1]) < ERROR_OFFSET  
+        parallel_to_wall = abs(ranges[SCAN_LEFT - 1] - ranges[SCAN_LEFT + 1]) < ERROR_OFFSET
 
         if parallel_to_wall:
             # all good, keep going
             print("all good, keep going")
             return 2 * NORMAL_LINEAR_VELOCITY, 0.0
-        
+
         # Robot positioning is bad
         # should go foward and rotate in order to move closer to the wall
 
@@ -185,12 +181,10 @@ class ScanToVelocityNode(Node):
             # rotate right
             print("rotate right")
             return NORMAL_LINEAR_VELOCITY, NORMAL_ANGULAR_VELOCITY
-        
+
         # rotate left
         print("rotate left")
         return NORMAL_LINEAR_VELOCITY, -1 * NORMAL_ANGULAR_VELOCITY
-    
-
 
     def scan_callback(self, scan_msg: LaserScan):
         cmd_vel = Twist()
@@ -223,21 +217,21 @@ class ScanToVelocityNode(Node):
                 cmd_vel.linear.x = 2 * NORMAL_LINEAR_VELOCITY
                 self.publisher.publish(cmd_vel)
                 return
-        
+
             # Closest wall is to the left, so turn right
             if closest_sensor > 180:
                 cmd_vel.angular.z = -1 * NORMAL_ANGULAR_VELOCITY
-            
+
             # Closest wall is to the right, so turn left
             else:
                 cmd_vel.angular.z = NORMAL_ANGULAR_VELOCITY
-            
+
             # Go straight, but realllly slow
             cmd_vel.linear.x = NORMAL_LINEAR_VELOCITY
 
         # ok, we're close to an obstacle, so let's follow it
         else:
-            print ("Close wall detected, trying to follow it")
+            print("Close wall detected, trying to follow it")
 
             cmd_vel.linear.x, cmd_vel.angular.z = self.follow_wall(scan_msg.ranges)
             self.publisher.publish(cmd_vel)
@@ -245,9 +239,10 @@ class ScanToVelocityNode(Node):
 
         # Apply safety limits to linear and angular velocities
         cmd_vel.linear.x = min(MAX_LINEAR_VELOCITY, cmd_vel.linear.x)
-        cmd_vel.angular.z = min(MAX_ANGULAR_VELOCITY, cmd_vel.angular.z)    
+        cmd_vel.angular.z = min(MAX_ANGULAR_VELOCITY, cmd_vel.angular.z)
 
         self.publisher.publish(cmd_vel)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -266,6 +261,6 @@ def main(args=None):
     rclpy.spin(node)
     rclpy.shutdown()
 
+
 if __name__ == '__main__':
     main()
-
